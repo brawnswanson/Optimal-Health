e8eab56306a135faa6fdbb7bb2dda28e90e32aa5
Daniel Pressner <daniel.pressner@gmail.com>
2022-09-05 17:56:02 +0200

    Initial Commit

---
+++ Optimal Health/Optimal_HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Optimal_HealthApp.swift
+//  Optimal Health
+//
+//  Created by Daniel Pressner on 05.09.22.
+//
+
+import SwiftUI
+
+@main
+struct Optimal_HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
